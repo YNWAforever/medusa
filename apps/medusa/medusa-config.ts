0cf9c6e7670d962971f7078eb032178a021f4b55
YNWAforever <laichiwillyjp@gmail.com>
2026-08-08 16:18:00 +0800

    feat: configure Medusa storefront URL

--- apps/medusa/medusa-config.ts
+++ apps/medusa/medusa-config.ts
@@ -19,6 +19,7 @@ module.exports = defineConfig({
   },
   admin: {
     disable: runtimeEnv.disableAdmin,
+    storefrontUrl: runtimeEnv.storefrontUrl,
   },
   modules: [
     ...buildInfrastructureModules(runtimeEnv),
